1a73fee9b5693caded94707b360dad2804ff0b5e
Jeet <dumbass.ass24@gmail.com>
2021-01-04 22:06:51 +0530

    init

---
+++ chaincode/fabcar/javascript/lib/fabcar.js
@@ -0,0 +1,39 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const { Contract } = require('fabric-contract-api');
+
+class FabCar extends Contract {
+
+    // initledger
+
+    async initLedger(ctx){
+        await ctx.stub.putState("test", "hello world!")
+        return "success"
+    }
+
+    // writedata 
+
+    async writeData(ctx, key, value){
+
+        await ctx.stub.putState(key,value)
+        return value;
+
+    }
+
+    // readdata
+
+    async(ctx, key){
+        var res = await ctx.stub.getState(key)
+        return res.toString()
+    }
+
+
+}
+
+module.exports = FabCar;
